f707093280cab4a1837b5b8f504c4f991f352a14
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-10-27 19:35:23 +1300

    update css links

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const herbRoutes = require('./routes/herbs')
 
 const server = express()
 
+server.use(express.static('public'))
+
 // Middleware
 server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 server.use(express.urlencoded({ extended: true }))
+
 
 // Routes
 server.use('/', herbRoutes)
